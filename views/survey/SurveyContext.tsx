86f21dce7609ded2cf68297dc5642d1b02d5252c
Julian <tipler.julian@gmail.com>
2023-08-20 12:37:20 -0500

    context addition of survey

--- views/survey/SurveyContext.tsx
+++ views/survey/SurveyContext.tsx
@@ -1,12 +1,15 @@
 import React, { createContext, useContext, useState } from "react";
 import { Prompt } from "../../entities/Prompt";
 import { Answer } from "../../entities/Answer";
+import { Survey } from "../../entities/Survey";
 
 const SurveyContext = createContext({} as SurveyContextChildren);
 
 export type AnswerWithoutId = Omit<Answer, "id">;
 
 type SurveyContextChildren = {
+  survey: Survey;
+  setSurvey: React.Dispatch<React.SetStateAction<Survey>>;
   prompts: Prompt[];
   setPrompts: React.Dispatch<React.SetStateAction<Prompt[]>>;
   answers: { [promptId: string]: AnswerWithoutId };
@@ -18,8 +21,9 @@ type SurveyContextChildren = {
 export function SurveyProvider({ children }: { children: React.ReactNode }) {
   const [prompts, setPrompts] = useState([]); // Will be fetched by a useEffect + firebase
   const [answers, setAnswers] = useState({}); // Initialize answers as an empty array
+  const [survey, setSurvey] = useState({});
 
-  const value = { prompts, setPrompts, answers, setAnswers };
+  const value = { prompts, setPrompts, answers, setAnswers, survey, setSurvey };
   return (
     <SurveyContext.Provider value={value}>{children}</SurveyContext.Provider>
   );
