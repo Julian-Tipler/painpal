922290239a1a99411d7f352cc71c4f2c15e97c72
Julian Tipler <juliantipler@FVFG16W2Q05F.local>
2023-09-23 15:33:42 +0200

    added space

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,6 +48,7 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
+
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 });
